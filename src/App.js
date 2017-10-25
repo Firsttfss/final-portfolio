807a5858dab1136a61c56897aafd4464a6564852
Firsttfss <first.sarita.fss@gmail.com>
2017-10-25 17:36:27 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Mycomponent from './component/MyComponent'
+import Header from './component/Header'
+import Box from './component/Box'
+import './static/bootstrap/bootstrap.min.css'
+
+
+const App =()=>(
+<div className="background-color">
+    <Header/>
+    <Box/>
+</div>
+);
+
+
+export default App;
